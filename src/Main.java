ff81e39f55d26cef78324f235e6c4086b26f94c0
JhaymesBach <jhaymesbachmann@gmail.com>
2024-08-19 21:47:30 -0300

    atividades3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        //Variáveis
+        // Criação de Variáveis
         String nomeProduto = "Notebook";
         double precoUnitario = 2500.00;
         int quantidadeComprada = 3;
         double taxaImposto = 0.10; // 10%
         double margemLucro = 0.20; // 20%
 
-        // Operações
+        // Operações Simples
         double valorTotalSemImpostos = precoUnitario * quantidadeComprada;
         double valorImposto = valorTotalSemImpostos * taxaImposto;
         double valorTotalComImpostos = valorTotalSemImpostos + valorImposto;
         double precoVendaSugerido = valorTotalComImpostos * (1 + margemLucro);
+
+        // Exibição de Resultados
+        System.out.println("Nome do Produto: " + nomeProduto);
+        System.out.println("Preço Unitário: R$ " + precoUnitario);
     }
 }

fdc36368800b46ab7cd98dbf5a89a2de647f6512
JhaymesBach <jhaymesbachmann@gmail.com>
2024-08-19 22:00:35 -0300

    atividades3

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-public class Main{
+import java.text.DecimalFormat;
+
+public class Main {
     public static void main(String[] args) {
         // Criação de Variáveis
         String nomeProduto = "Notebook";
@@ -13,9 +15,15 @@ public class Main{
         double valorTotalComImpostos = valorTotalSemImpostos + valorImposto;
         double precoVendaSugerido = valorTotalComImpostos * (1 + margemLucro);
 
-        // Exibição de Resultados
-        System.out.println("Nome do Produto: " + nomeProduto);
-        System.out.println("Preço Unitário: R$ " + precoUnitario);
+        DecimalFormat df = new DecimalFormat("R$ #,##0.00");
+        String valorTotalComImpostosFormatado = df.format(valorTotalComImpostos);
+        String precoVendaSugeridoFormatado = df.format(precoVendaSugerido);
+        String mensagemFinal = "Produto: " + nomeProduto + ", Valor Total com Impostos: "
+                + valorTotalComImpostosFormatado + ", Preço de Venda Sugerido: " + precoVendaSugeridoFormatado;
 
+        System.out.println("Nome do Produto: " + nomeProduto);
+        System.out.println("Preço Unitário: R$ " + df.format(precoUnitario));
+        System.out.println("Quantidade Comprada: " + quantidadeComprada);
+        System.out.println(mensagemFinal);
     }
 }
